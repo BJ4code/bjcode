a5fce490264c10d00592c72a7b91146498382dce
BJ4code <business.aljuma@gmail.com>
2026-03-19 07:12:51 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Admin from "./Admin.jsx";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const path = window.location.pathname;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {path === "/admin" ? <Admin /> : <App />}
   </React.StrictMode>
 );
